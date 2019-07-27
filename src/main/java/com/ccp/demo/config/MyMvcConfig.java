a24dca64e6a2a3712a9b6213e12790bf23b48359
CCP <1028524069@qq.com>
2019-07-27 22:25:37 +0800

    mvcConfig

---
+++ src/main/java/com/ccp/demo/config/MyMvcConfig.java
@@ -0,0 +1,15 @@
+package com.ccp.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/test").setViewName("test");
+    }
+
+}
